2b69fc74fd0a3a0c08482f18358a64ea991e48c3
priyanka1012-singhal <priyanka.singhal@sjsu.edu>
2019-01-28 17:00:52 -0800

    Initial Commit

---
+++ tipcal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipcal
+//
+//  Created by Priyanka  Singhal on 1/28/19.
+//  Copyright © 2019 Priyanka  Singhal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
